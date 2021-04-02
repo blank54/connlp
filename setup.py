1a85960bdd9471dba67613683ba89481847e0412
SeonghyeonMoon <blank54@snu.ac.kr>
2021-04-02 13:56:13 +0900

    update connlp: 0.0.6

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='constrnlp',
-    version='0.0.5',
-    license='GPL',
+    name='connlp',
+    version='0.0.6',
+    license='',
     author='Seonghyeon Boris Moon',
     author_email='boris.moon514@gmail.com',
     description='A bunch of python codes to analyze text data in the construction industry. Mainly reconstitute the pre-exist python libraries for Natural Language Processing (NLP)',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/blank54/constrnlp_pypi.git',
+    url='https://github.com/blank54/connlp.git',
     packages=find_packages(),
     classifiers=[],
 )
