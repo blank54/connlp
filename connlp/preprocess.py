6c777e05117fa363a845bc529ba82d45a84eefd8
SeonghyeonMoon <blank54@snu.ac.kr>
2021-04-02 14:46:37 +0900

    add documentation: preprocess

--- connlp/preprocess.py
+++ connlp/preprocess.py
@@ -5,32 +5,50 @@
 import re
 
 
-class EnglishTokenizer:
-    def __init__(self, **kwargs):
-        self.n = kwargs.get('n', 1) # ngram size
-
-    def tokenize(self, text):
-        result = [w for w in re.split(' |  |\n', text) if w]
-        return result
-
-
 class Normalizer:
-    def __init__(self, **kwargs):
-        self.do_marking = kwargs.get('do_marking', False)
+    '''
+    A class used to normalize text with predetermined character replacement rules.
+
+    Attributes
+    ----------
+    do_lower : Boolean
+        | To lower the text or not. (default : True)
+    do_marking : Boolean
+        | To mark several tokens to predetermined entities or not. (default : False)
+        | The predetermined entities include "LINK", "REF", and "NUM".
+
+    Methods
+    -------
+    normalize
+        | Normalizes the input text and returns a clean text.
+    '''
+
+    def __init__(self, do_lower=True, do_marking=False, **kwargs):
+        self.do_lower = do_lower
+        self.do_marking = do_marking
 
     def __remove_trash_char(self, text):
-        text = re.sub('[^ \'\?\./0-9a-zA-Zㄱ-힣\n]', '', text.lower())
+        text = re.sub('[^ \'\?\./0-9a-zA-Zㄱ-힣\n]', '', text)
 
+        # Remove debris from format conversion
         text = text.replace(' / ', '/')
         text = re.sub('\.+\.', ' ', text)
         text = text.replace('\\\\', '\\').replace('\\r\\n', '')
-
         text = text.replace('\n', '  ')
         text = re.sub('\. ', '  ', text)
         text = re.sub('\s+\s', ' ', text).strip()
+
+        # Lower the text
+        if self.do_lower:
+            text = text.lower()
+        else:
+            pass
         
+        # Remove endmarks
         if text.endswith('\.'):
             text = text[:-1]
+        else:
+            pass
             
         return text
 
@@ -42,10 +60,67 @@ class Normalizer:
                 sent[i] = 'REF'
             elif re.match('\d', str(w)):
                 sent[i] = 'NUM'
+            else:
+                continue
+
         return sent
 
     def normalize(self, text):
+        '''
+        A method to normalize the input text.
+
+        Attributes
+        ----------
+        text : str
+            | An input text in str type.
+        '''
+
         if self.do_marking:
             text = self.__marking(text)
+        else:
+            pass
             
         return self.__remove_trash_char(text)
+
+    ## TODO: udpate character replacement rules
+    # def update_char_list(self, input_char_list, verbose=False):
+    #     cnt_before = len(self.remain_char_list)
+    #     self.remain_char_list = list(set(self.remain_char_list.extend(input_char_list)))
+    #     cnt_after = len(self.remain_char_list)
+
+    #     if verbose:
+    #         print('|Update remain_char_list: [{}] chars --> [{}] chars'.format(cnt_before, cnt_after))
+
+
+class EnglishTokenizer:
+    '''
+    A class to tokenize an English sentence.
+
+    Attributes
+    ----------
+    n : int
+        | ngram size. (default : 1)
+
+    Methods
+    -------
+    tokenize
+        | tokenizes the input sentence.
+    '''
+
+    def __init__(self, **kwargs):
+        self.n = kwargs.get('n', 1) # ngram size
+
+    def tokenize(self, text):
+        '''
+        A method to tokenize the input text.
+
+        Attributes
+        ----------
+        text : str
+            | An input text in str type.
+        '''
+
+        result = [w for w in re.split(' |  |\n', text) if w]
+        return result
+
+    ## TODO: split based on ngrams
