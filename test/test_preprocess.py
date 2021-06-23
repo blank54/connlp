6d4d077e4b471ef5723c60fd0f20bd54dd9eea3e
Sehwan Chung <hwani751@snu.ac.kr>
2021-06-23 15:55:52 +0900

    update KoreanTokenizer

--- test/test_preprocess.py
+++ test/test_preprocess.py
@@ -8,10 +8,10 @@ import sys
 file_path = os.path.dirname(os.path.abspath(__file__))
 config_path = os.path.sep.join(file_path.split(os.path.sep)[:-1])
 sys.path.append(config_path)
-from connlp.preprocess import EnglishTokenizer, Normalizer
+from connlp.preprocess import EnglishTokenizer, Normalizer, KoreanTokenizer
 tokenizer = EnglishTokenizer()
 normalizer = Normalizer()
-
+kor_tokenizer = KoreanTokenizer(min_frequency=0)
 
 ## Preprocess
 # Normalizer
@@ -30,6 +30,16 @@ def test_english_tokenizer():
         tokenized_doc = tokenizer.tokenize(text=doc)
         print(tokenized_doc)
 
+# Korean Tokenizer
+def test_korean_tokenizer(kor_docs):
+    print('\nTRAINING Korean Tokenizer...')
+    kor_tokenizer.train(kor_docs)
+
+    print()
+
+    for doc in kor_docs:
+        tokenized_doc = kor_tokenizer.tokenize(doc)
+        print(tokenized_doc)
 
 ## Run
 if __name__ == '__main__':
@@ -37,3 +47,9 @@ if __name__ == '__main__':
 
     test_normalizer()
     test_english_tokenizer()
+
+    kor_docs = ['코퍼스의 첫 번째 문서입니다.', '두 번째 문서입니다.', '마지막 문서']
+
+    print('\nKorean Tokenizer Test')
+    print(kor_docs)
+    test_korean_tokenizer(kor_docs)
